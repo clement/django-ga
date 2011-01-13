540bf25ea17792b32885b6d821cebd256dbd6abc
Clément <clement.nodet@gmail.com>
2011-01-13 10:56:57 +0700

    add support for google site verification

--- ga/settings.py
+++ ga/settings.py
@@ -1,2 +1,3 @@
 from django.conf import settings
 ANALYTICS_CODE = getattr(settings, 'GA_ANALYTICS_CODE', None)
+VERIFICATION_CODE = getattr(settings, 'GA_VERIFICATION_CODE', None)
--- ga/templatetags/analytics.py
+++ ga/templatetags/analytics.py
@@ -6,3 +6,7 @@ from .. import settings
 @register.inclusion_tag('ga/analytics.html')
 def analytics(code=None):
     return {'code': code or settings.ANALYTICS_CODE}
+
+@register.inclusion_tag('ga/verification.html')
+def verification(code=None):
+    return {'code': code or settings.VERIFICATION_CODE}
